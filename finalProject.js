de54820f5351f402adedc5057bf2c19b6b457798
Cris Vizcaino <cris.vizcaino@live.com>
2023-08-06 14:27:12 -0700

    fixed code

--- finalProject.js
+++ finalProject.js
@@ -21,51 +21,42 @@ const tax = 1.20;
 
 // Implement getPrices()
 function getPrices(taxBoolean) {
-    for (let index = 0; index < dishData.length; index++) {
-        var element = dishData[index];
-        finalPrice = element.price;
-        if (taxBoolean) {
-            finalPrice = element.price * tax;
-        }
-        else if (!taxBoolean){
-            element.price = finalPrice;
-        }
-        else{
+    for (var dish of dishData) {
+        let finalPrice;
+        if (taxBoolean == true) {
+            finalPrice = dish.price * tax;
+        } else if (taxBoolean == false) {
+            finalPrice = dish.price;
+        } else {
             console.log("You need to pass a boolean to the getPrices call!");
+            return;
         }
-
-        console.log("Dish: ", element.name, "Price: $", finalPrice)
+        console.log("Dish: " + dish.name + " Price: $" + finalPrice);
     }
 }
 
 // Implement getDiscount()
-function getDiscount(taxBoolean,guests) {
+function getDiscount(taxBoolean, guests) {
     getPrices(taxBoolean);
-
-
-    if (typeof guests === 'number' && guests > 0 && guests < 30) {
-    // Do something if the number is in range
-        discount = 0;
+    if (typeof(guests) == 'number' && guests > 0 && guests < 30) {
+        let discount = 0;
         if (guests < 5) {
             discount = 5;
-            
-        } else if(guests >= 5) {
-            discount = 10
-            
+        } else if (guests >= 5) {
+            discount = 10;
         }
-        console.log('Discount is $' + discount);
+        console.log('Discount is: $' + discount);
     } else {
-    // Do something else if the number is not in range
-        console.log('The second argument must be a number greate between 0 and 30');
+        console.log('The second argument must be a number between 0 and 30');
     }
-
-
-
 }
+
 
 // Call getDiscount()
 getDiscount(true, 2)
-getDiscount(false, 10)
+getDiscount()
+/getDiscount(false, 10)
 getDiscount(false,4)
 getDiscount(true,13)
 getDiscount(true,-5)
+getDiscount(2,3)
